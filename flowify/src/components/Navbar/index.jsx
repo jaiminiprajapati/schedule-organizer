ed1b59df60938fc78df1bc81ea7ec71cb75adafc
jaiminiprajapati <151653602+jaiminiprajapati@users.noreply.github.com>
2024-03-21 13:04:54 +0000

    Update index.jsx

--- flowify/src/components/Navbar/index.jsx
+++ flowify/src/components/Navbar/index.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href="https://flowify.netlify.app/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={Logo} className="h-8" alt="Flowbite Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-second-color">Flowify</span>
         </a>
